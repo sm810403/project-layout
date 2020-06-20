9b00e5fbd69a4aae80a28689b912bb12280902b6
JaeIn <sm810403@hotmail.com>
2020-06-19 17:15:59 -0700

    6th commit
    toggle btn added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// dark mode button
 let button = document.querySelector('.btn');
 let balls = document.querySelector('.btn-ball');
 
@@ -9,5 +10,16 @@ button.addEventListener('click', ()=> {
         balls.classList.remove('ball');
         document.body.classList.remove('all');
     }
+})
 
+//nav dropdown
+let navBox = document.querySelector('.nav-box');
+let toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', ()=>{
+    if (!navBox.classList.contains('nav-box2')){
+        navBox.classList.add('nav-box2');
+    } else {
+        navBox.classList.remove('nav-box2');
+    }
 })
