28e61eae0037b990fe87b8a2e610bd9108ee4384
JaeIn <sm810403@hotmail.com>
2020-06-21 10:17:36 -0700

    fix
    background default fixed

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 let button = document.querySelector('.btn');
 let balls = document.querySelector('.btn-ball');
 
+window.addEventListener('load',function(){
+    let defaultMode = document.body;
+    defaultMode.classList.remove('all');
+});
+
 button.addEventListener('click', ()=> {
-    if (!balls.classList.contains('ball')){
+    if (!balls.classList.contains('ball')){    
         balls.classList.add('ball');
         document.body.classList.add('all');
     } else {
         balls.classList.remove('ball');
         document.body.classList.remove('all');
     }
+
 })
+
+
+
 
 //nav dropdown
 let navBox = document.querySelector('.nav-box');
@@ -23,3 +32,51 @@ toggle.addEventListener('click', ()=>{
         navBox.classList.remove('nav-box2');
     }
 })
+
+
+    // const types = ['Journey','of','Self Taught Web Developer'];
+    // let count = 0;
+    // let index = 0;
+    // let words = '';
+    // let letters = '';
+
+    // (function typing() {
+    //     if (count === types.length) {
+    //         count = 0;
+    //     }
+    //     words = types[count];
+    //     letters = words.slice(0, ++index);
+    //     document.querySelector('.type').textContent = letters;
+    //     if (letters.length === words.length) {
+    //         count++;
+    //         index = 0; 
+              
+    //     }
+    //     setTimeout(typing,400);
+    // })();
+   
+// let textMe = document.querySelector('.textMe');
+// let aboutText = textMe.textContent;
+// let splitText = aboutText.split("");
+// textMe.textContent = "";
+// for (let i = 0; i < splitText.length; i++){
+//     textMe.innerHTML += "<span>" + splitText[i] +"</span>";
+// }
+
+// let char =0
+// let timer = setInterval(setEffect, 70);
+
+// function setEffect() {
+//     let spans = textMe.querySelectorAll("span")[char];
+//     spans.classList.add("fade");
+//     char++;
+//      if (char === splitText.length) {
+        
+//         complete()
+//         return;
+//      }
+// };
+// function complete(){
+//     clearInterval(timer);
+//     timer = null;
+// };
